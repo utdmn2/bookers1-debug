667ce5561eb4828e0481dd9733e90255ac4b121b
utdmn2 <utdmn2@gmail.com>
2021-09-26 02:16:54 +0000

    GitへのPush失敗によるやり直し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
+    if @book.update
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
